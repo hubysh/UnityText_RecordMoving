e15043a57efb806e4eb19ce61b7d599cb21f8934
hubysh <design.ysh@gmail.com>
2016-05-30 14:32:16 +0900

    Init

---
+++ Assets/03_Scripts/Scripts_Common/Singleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+using System.Collections.Generic;
+
+/// <summary>
+/// 전역에서 접근 가능한 클래스
+/// MonoBehaviour을 상속한 이유는 Inspetor에서 정보를 실시간으로 보기 위함
+/// 상속 없을 경우, 객체 핸들 얻지 못함.
+/// </summary>
+public class Singleton : MonoBehaviour
+{
+	private static Singleton instance = null;
+
+	public GameObject txtResult;
+
+	public static Singleton Instance
+	{
+		get
+		{
+			if (instance == null)
+			{
+				GameObject objSingleton = new GameObject("objSingleton");
+				instance = objSingleton.AddComponent(typeof(Singleton)) as Singleton;
+			}
+			return instance;
+		}
+	}
+
+	void Awake()
+	{
+		txtResult = GameObject.Find("txtResult");
+	}
+
+}
---
+++ Assets/03_Scripts/Scripts_UI/TestSingleton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestSingleton : MonoBehaviour
+{
+	bool isActive = true;
+
+	void TestSingletonScript()
+	{
+		if (isActive)
+		{
+			Singleton.Instance.txtResult.SetActive(isActive);
+			isActive = false;
+		}
+		else
+		{
+			Singleton.Instance.txtResult.SetActive(isActive);
+			isActive = true;
+		}
+		
+	}
+}
