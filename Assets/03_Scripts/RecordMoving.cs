9ebdfefc990ce1f7a714d4a11d737e2264576409
hubysh <design.ysh@gmail.com>
2016-05-30 18:07:02 +0900

    add - xml 파일 생성 완료

--- Assets/03_Scripts/RecordMoving.cs
+++ Assets/03_Scripts/RecordMoving.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
+using System.Xml;
 
 public class RecordMoving : MonoBehaviour
 {
@@ -31,9 +32,7 @@ public class RecordMoving : MonoBehaviour
 			// 리스트에 순간 위치 추가
 			lstObjectMovement.Add(tempObjectMovement);
 			
-
-			Debug.Log(tempObjectMovement.pos.ToString());
-			Debug.Log(lstObjectMovement.Count.ToString());
+			Debug.Log("시퀀스 : "+tempObjectMovement.sequence.ToString() + " / 위치 : " + tempObjectMovement.pos.ToString() + " / 각도 : "+tempObjectMovement.rot.ToString());
 
 			// sequence 증가
 			cnt++;
@@ -50,8 +49,22 @@ public class RecordMoving : MonoBehaviour
 		recording = false;
 	}
 
-	void SaveList()
+	void SaveListToXml()
 	{
+		// 움직임 기록 xml 파일
+		XmlDocument xmlMovements = new XmlDocument();
+
+		XmlElement lv1Main = xmlMovements.CreateElement("Main");
+		xmlMovements.AppendChild(lv1Main);
+
+		XmlElement lv2Sequence= xmlMovements.CreateElement("Sequence");
+		lv1Main.AppendChild(lv2Sequence);
+		XmlElement lv2Position = xmlMovements.CreateElement("Position");
+		lv1Main.AppendChild(lv2Position);
+		XmlElement v2Rotation = xmlMovements.CreateElement("Rotation");
+		lv1Main.AppendChild(v2Rotation);
+
+		xmlMovements.Save("C:/Users/ysh/Desktop/xmlMovements.xml");        // XML 파일 저장
 
 	}
 
